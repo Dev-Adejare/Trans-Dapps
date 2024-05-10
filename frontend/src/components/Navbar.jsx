f9f63dd45f9f7efa147a6ba936d7d6d0ddeb73ef
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-10 13:11:18 +0100

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { HiMenuAlt4 } from 'react-icons/hi'
+import { AiOutlineClose} from 'react-icons/ai'
+import Logo from '../assets/adejare.jpg'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+     <div>
+     <Image
+      src={Logo}
+      width={100}
+      height={100}
+      alt="Picture of the author"
+    />
+     </div>
+    </nav>
   )
 }
 
