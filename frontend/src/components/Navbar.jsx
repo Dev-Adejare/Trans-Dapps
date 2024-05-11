b2fd8008f2e465a3b7d54a41c3be03cd535fac1a
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-11 12:23:51 +0100

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+'use client'
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import Logo from "../assets/adejare.jpg";
 import Image from "next/image";
+import { useState } from "react";
 
 const NavbarItems = ({ title, classProps }) => {
   return <li className={`mx-4 cursor pointer ${classProps}`}>{title}</li>;
 };
 
-const Navbar = () => {
-  const {toggleMenu, setToggleMenu} = useState(false)
+const NavBar = () => {
+
+  const { toggleMenu, setToggleMenu} = useState(false)
+
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -29,23 +32,36 @@ const Navbar = () => {
       <div>
         {toggleMenu ? (
           <AiOutlineClose
-          fontSize={28}
-          className="text-white md:hidden cursor-pointer"
-          onClick={() => setToggleMenu(true)}
-          
+            fontSize={28}
+            className="text-white md:hidden cursor-pointer"
+            onClick={() => setToggleMenus(true)}
           />
-
         ) : (
           <HiMenuAlt4
-          fontSize={28}
-          className="text-white md:hidden cursor-pointer"
-          onClick={() => setToggleMenu(true)}
+            fontSize={28}
+            className="text-white md:hidden cursor-pointer"
+            onClick={() => setToggleMenu(true)}
           />
-          
+        )}
+
+        {toggleMenu && (
+          <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
+            <li>
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+            </li>
+
+            {["Market", "Exchange", "Rates", "Trade", "Account"].map((item, index) => (
+              <NavbarItems
+                key={item + index}
+                title={item}
+                classProps="my-2 text-lg"
+              />
+            ))}
+          </ul>
         )}
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+export default NavBar;
--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Welcome = () => {
   return (
-    <div>
-      
+    <div className="flex w-full justify-center items-center">
+      <div className="flex mf:flex-row flex-column items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+            Send Crypto <br /> Accross The World
+          </h1>
+          <p>
+            Explore the crypto world
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
